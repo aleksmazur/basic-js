b1b3bea43734344c1bc51f343fed6256b7983b0a
Aleksandra <aleksandra@MacBook-Air-Aleksandra.local>
2022-10-14 23:26:25 +0300

    feat:add cipher solution

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,15 +20,55 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
+  constructor(direction) {
+    this.direction = true;
+    if (direction === false) {this.direction = false}
+  }
 
   encrypt(message, key) {
-    if (message === undefined || key === undefined) {}
-    throw new Error("Incorrect arguments!");
-  }
-  decrypt(encryptedMessage, key) {
-    if (encryptedMessage === undefined || key === undefined) {}
-    throw new Error("Incorrect arguments!");
+    if (message === undefined || key === undefined) {throw new Error("Incorrect arguments!");}
+    
+    let encryptedMessage = '';
+    let keyindex = 0;
 
+    for (let i = 0; i < message.length; i++) {
+      let letter = message[i].toUpperCase();
+      if (letter.charCodeAt() > 64 && letter.charCodeAt() < 91) {
+        let mi = (letter.charCodeAt() - 65);
+        let ki = (key[keyindex % key.length].toUpperCase().charCodeAt() - 65);
+        let encryptLetter = (mi + ki) % 26;
+        encryptedMessage += String.fromCharCode(encryptLetter + 65);
+        keyindex++;
+      } else {
+        encryptedMessage += letter;
+      }
+    }
+    if (this.direction === true) { return encryptedMessage; } else {
+      return encryptedMessage.split('').reverse().join('');
+    }
+  }
+
+  decrypt(encryptedMessage, key) {
+    if (encryptedMessage === undefined || key === undefined) {throw new Error("Incorrect arguments!");}
+    
+    let message = '';
+    let keyindex = 0;
+
+    for (let i = 0; i < encryptedMessage.length; i++) {
+      let encryptLetter = encryptedMessage[i];
+      if (encryptLetter.charCodeAt() > 64 && encryptLetter.charCodeAt() < 91) {
+        let ci = (encryptLetter.charCodeAt() - 65);
+        let ki = (key[keyindex % key.length].toUpperCase().charCodeAt() - 65);
+        let letter = (ci - ki + 26) % 26;
+        message += String.fromCharCode(letter + 65);
+        keyindex++;
+      } else {
+        message += encryptLetter;
+      }
+    }
+    if (this.direction === true) { return message; } else {
+      return message.split('').reverse().join('');
+    }
   }
 }
 
